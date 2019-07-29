9f35d793549370e25f38233d3448d419b0a40c2d
guangjiazheng02 <guangjiazheng02@meituan.com>
2019-07-29 13:51:10 +0800

    push1

--- src/main/java/com/qa/Case/test.java
+++ src/main/java/com/qa/Case/test.java
@@ -1,7 +1,17 @@
 package com.qa.Case;
 
+import java.math.BigInteger;
+
 /**
  * Created by guangjiazheng on 2019/3/11.
  */
 public class test {
+    public static void main(String[] args){
+        String a = "123";
+        String b = "321";
+
+        String c = new BigInteger(a).add(new BigInteger(b)).toString();
+        System.out.println(c);
+
+    }
 }
